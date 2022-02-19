0aa311dd8d7a0ab3e13de4aeec9ddaed228ee28e
Luhanyo Tando Nkohla <60959721+lukhanyoThando@users.noreply.github.com>
2022-02-19 02:19:57 +0200

    Birthday
    updates

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        id: 1,
+        name : 'Tom Holland',
+        age: 25,
+        image: 'https://wallpapercave.com/wp/wp4585578.jpg'
+
+    },
+    {
+        id: 2,
+        name : 'Andrew Garfield',
+        age: 38,
+        image:'https://i.pinimg.com/originals/8e/38/f6/8e38f694415f4d48e5ff51a265cd6ed7.jpg'
+    },
+    {
+        id: 3,
+        name : 'Tobey Maguire',
+        age : 46,
+        image: 'https://wallsdesk.com/wp-content/uploads/2016/11/tobey-maguire-full-HD.jpg'
+    }
+];
